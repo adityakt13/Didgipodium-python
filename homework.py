344c827151c2024a3d66950714356abe785f75bb
adityakt13 <adityakt13@gmail.com>
2022-11-20 17:40:31 +0530

    No. of characters
    Checked if the number of characters in the provided input is odd or even.

---
+++ homework.py
@@ -0,0 +1,7 @@
+# check the length of name if the characters are odd or even
+
+name = input("Enter your name : ")
+if len(name)%2==0:
+    print ("number of characters is even")
+else:
+    print ("number of characters is odd")
