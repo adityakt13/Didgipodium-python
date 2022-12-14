f873028152ac14ab264a196121d3b70ebcd04944
adityakt13 <adityakt13@gmail.com>
2022-12-14 11:49:39 +0530

    list updated

---
+++ List/list_operations.py
@@ -0,0 +1,17 @@
+x = [1,2,3,4,5]
+# duplicating a list
+y = x * 5
+print(y)
+
+# concatenating a list
+a = [1,2,3]
+b = [4,5,6]
+c = [7,8,9]
+print(a+b+c)
+
+# deleting a list
+del (x, y) # remove x and y from memory
+
+# deleting element from list
+del (a[0]) # removes first element of a 
+print ("abc = ",a+b+c)
