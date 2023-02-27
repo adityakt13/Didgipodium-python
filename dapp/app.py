9d142841fefeecdeb33e0264b00d70bc3ba926d8
adityakt13 <118040130+adityakt13@users.noreply.github.com>
2023-02-27 22:45:18 +0530

    streamlit

--- dapp/app.py
+++ dapp/app.py
@@ -19,3 +19,12 @@ def load_dataset():
     df = pd.read_excel("dapp/canada.xlsx", sheet_name=1, skiprows=20, skipfooter=2)
     df.drop(['AREA', 'REG', 'DEV', 'Type', 'Coverage'], axis=1, inplace=True)
     df.rename(columns={'OdName':'Country', 'AreaName':'Continent', 
+                       'RegName':'Region', 'DevName':'Status'}, inplace=True)
+    df.set_index('Country', inplace=True)
+    df['Total'] = df[years].sum(axis=1)
+    return df
+
+with st.spinner('Loading data...'):
+    df = load_dataset()
+    # st.balloons()
+
