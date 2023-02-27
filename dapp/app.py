ba206eac108861a2eb27a754295817de461a13e2
adityakt13 <118040130+adityakt13@users.noreply.github.com>
2023-02-27 22:44:06 +0530

    streamlit

---
+++ dapp/app.py
@@ -0,0 +1,9 @@
+# 1. pip install streamlit --upgrade
+# 2. create a new folder called 📂dapp
+# 3. create a new file called app.py
+# 4. copy the canada.xlsx file to the 📂dapp folder
+# 5. streamlit run dapp/app.py
+
+import streamlit as st
+import pandas as pd
+import numpy as np
